32abef48317b4fc4ecae13b88d4e55ea2232ddad
Bastian Rieck <bastian@rieck.ru>
2015-01-11 19:18:04 +0100

    Incipit

---
+++ lb.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+#
+# lb.py --- Little brother, a script for analysing who accesses your website
+
+import collections
+import ipaddress
+import re
+import sys
+
+def readNetworks(filename):
+  """
+  Reads a list of networks from a file. The file should contain an IP address
+  or an IP network with a bitmask, followed by a description. The description
+  is to be separated from the IP network by at least one whitespace character.
+
+  The function will ignore any lines that do not seem to contain an IP address
+  or an IP network.
+  """
+
+  networks = list()
+
+  with open(filename) as f:
+    for line in f:
+      matches = re.match(r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(?:/\d{1,3})?)(?:\s+)(.*$)", line)
+      if matches:
+        address     = matches.group(1)
+        description = matches.group(2)
+
+        networks.append( (ipaddress.ip_network(address), description) )
+
+  return networks
+
+def readLog(filename):
+  """
+  Reads an Apache log. The file is supposed to contain an IP address as its
+  first token. The function will only look for said IP address and count how
+  often it occurs in the log file.
+  """
+
+  addresses = list()
+
+  with open(filename) as f:
+    for line in f:
+      m = re.match(r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})", line)
+      if m:
+        addresses.append( ipaddress.ip_address(m.group(1)) )
+
+  return collections.Counter(addresses)
+
+#
+# main
+#
+
+if __name__ == "__main__":
+  if len(sys.argv) < 3:
+    print("Usage: lb.py NETWORKS LOG\n")
+    sys.exit(-1)
+
+  networks  = readNetworks(sys.argv[1])
+  addresses = readLog(sys.argv[2])
+
+  for address, count in addresses.most_common():
+    for network, description in networks:
+      if address in network:
+        print("Counted %d visits from %s (%s)" % (count, address, description))
